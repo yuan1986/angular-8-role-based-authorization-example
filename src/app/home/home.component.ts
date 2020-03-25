c8fa9f654c5fece150a6f866fde8b7baec05cb84
yuan1986 <292549001@qq.com>
2020-03-25 21:39:15 +0800

    test

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,26 +3,36 @@ import { first } from 'rxjs/operators';
 
 import { User } from '@app/models';
 import { UserService, AuthenticationService } from '@app/services';
+import { Router } from '@angular/router';
 
 @Component({ templateUrl: 'home.component.html' })
 export class HomeComponent {
-    loading = false;
-    currentUser: User;
-    userFromApi: User;
+  loading = false;
+  currentUser: User;
+  userFromApi: User;
 
-    constructor(
-        private userService: UserService,
-        private authenticationService: AuthenticationService
-    ) {
-        this.currentUser = this.authenticationService.currentUserValue;
-    }
+  constructor(
+    private router: Router,
+    private userService: UserService,
+    private authenticationService: AuthenticationService
+  ) {
+    this.currentUser = this.authenticationService.currentUserValue;
+  }
 
-    // tslint:disable-next-line: use-lifecycle-interface
-    ngOnInit() {
-        this.loading = true;
-        this.userService.getById(this.currentUser.id).pipe(first()).subscribe(user => {
-            this.loading = false;
-            this.userFromApi = user;
-        });
-    }
+  // tslint:disable-next-line: use-lifecycle-interface
+  ngOnInit() {
+    this.loading = true;
+    this.userService
+      .getById(this.currentUser.id)
+      .pipe(first())
+      .subscribe(user => {
+        this.loading = false;
+        this.userFromApi = user;
+      });
+  }
+
+  logout() {
+    this.authenticationService.logout();
+    this.router.navigate(['/login']);
+  }
 }
